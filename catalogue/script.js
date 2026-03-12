e0e33c644f06b3f8ffaf7df6090c663a932acc69
moubinesk <mails.moubinesk@gmail.com>
2026-03-12 12:00:08 +0100

    Redirections posters et logo

--- catalogue/script.js
+++ catalogue/script.js
@@ -30,7 +30,7 @@ Papa.parse(csvUrl, {
                 // image capsule
                 const img = card.querySelector("img");
                 if(img && item.poster){
-                    img.src = "posters/" + id + ".jpg";
+                    img.src = "../posters/" + id + ".jpg";
                 }
 
                 const title = card.querySelector(".Titre");
@@ -74,7 +74,7 @@ Papa.parse(csvUrl, {
             // IMAGE POSTER
     const poster = document.getElementById("poster");
     if(poster){
-        poster.src = "posters/" + item.ID + ".jpg";
+        poster.src = "../posters/" + item.ID + ".jpg";
         poster.alt = item.Titre;
     }
     // LIEN DE VISIONNAGE
